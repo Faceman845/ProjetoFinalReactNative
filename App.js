b0fef963f221ada2b23ee07c3370320b13d059cd
Gustavo Santos <faceman8456@gmail.com>
2025-06-03 17:12:14 -0300

    correção app.js
    - safeAreaView para evitar que o conteúdo saia da tela

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { AuthProvider } from './context/AuthContext';
 import Routes from './navigation/Routes';
-
 export default function App() {
   return (
-    <AuthProvider>
-      <Routes />
-    </AuthProvider>
+    <SafeAreaView style={styles.appContainer}>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+});
